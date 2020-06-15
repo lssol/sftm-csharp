705540bae06803288ec885d56fc86ec2d6e96fc1
Sacha Brisset <sbrisset@mantu.com>
2020-06-15 07:15:18 +0200

    Created project for visualization

---
+++ tree-matching-csharp.Visualization/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tree_matching_csharp.Visualization.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
